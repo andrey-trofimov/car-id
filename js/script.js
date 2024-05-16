8c6849be5cbeaa1d483039284fd3e1e45964bf98
Andrey <va_bank@mail.ru>
2024-05-17 00:20:22 +0300

    add sending messages feature

--- js/script.js
+++ js/script.js
@@ -1,86 +1,129 @@
-import { modal } from "./modal.js";
+import { sentMessegeNotice, waitingForReadingNotice, readMessegeNotice } from "./systemMessages.js";
 
 const carEventsContainer = document.querySelector("#radio-btns");
 const submitBtn = document.querySelector("#submit");
 const sectionSendMsg = document.querySelector("#send-msg");
 const modalContainer = document.querySelector(".modal-container");
-
 let carEventList = [];
-
-submitBtn.addEventListener("click", (event) => readMsg(event), false);
+let notificationId = 0;
+let timeoutId = 0;
 
 buildEventList();
 
 async function buildEventList() {
-    // const fetchUrl = `https://api.allorigins.win/get?url=${encodeURIComponent(
-    //     "https://car-id.ru/api/report/get_all_reasons"
-    // )}`;
     const fetchUrl = "https://car-id.ru/api/report/get_all_reasons";
 
     try {
         let response = await fetch(fetchUrl);
         if (response.ok) {
-            let eventList = await response.json();
-            let carEvents = await JSON.parse(eventList.contents);
-            carEventList = [...carEvents];
+            carEventList = [...(await response.json())];
 
-            carEventsContainer.innerHTML = carEventList.map((carEvent) => addCarEventRadioBtn(carEvent)).join("");
+            carEventsContainer.innerHTML = carEventList.map((carEvent) => createCarEventRadioBtn(carEvent)).join("");
             submitBtn.removeAttribute("disabled", "");
-            console.log(carEventList);
+            submitBtn.addEventListener("click", (event) => readMsg(event), false);
         } else {
-            console.log("Ошибка соединения с сервером: ", response.status);
+            showError(response.status);
         }
     } catch (err) {
-        console.log("Ошибка соединения с сервером: ", err);
+        showError(err);
     }
 }
 
-function addCarEventRadioBtn(carEvent) {
+function createCarEventRadioBtn(carEvent) {
     return `<label class="custom-radio">${carEvent.description}<input type="radio" name="carEvent" id=${carEvent.id} /></label>`;
 }
 
 function readMsg(event) {
-    if (carEventsContainer.innerHTML === "") return;
     event.preventDefault();
     const userChoice = document.querySelector("input:checked");
     if (userChoice === undefined || userChoice === null) return;
-    carEventList.map((msg) => (+msg.id == +userChoice.id ? sendMsg(msg.id) : false));
+    carEventList.map((event) => (+event.id === +userChoice.id ? sendMsg(event.id) : false));
 }
 
-async function sendMsg(msg) {
-    modalContainer.innerHTML = modal;
+async function sendMsg(eventId) {
+    // let windowLocationPathname = window.location;
+    const windowLocationPathname = "/qr/74b82e70-c612-4b7c-995a-e3bb88d3ad4a";
+    const fetchUrl = "https://car-id.ru/api/report/send";
 
-    const closeBtn = document.querySelector("#close-btn");
-    closeBtn.addEventListener("click", (event) => closeModal(event));
-
-    console.log(msg);
-
-    const fetchUrl = `https://api.allorigins.win/get?url=${encodeURIComponent(
-        "https://car-id.ru/api/report/get_all_reasons"
-    )}`;
-    // const fetchUrl = "https://car-id.ru/api/report/get_all_reasons";
+    let body = {
+        qr_id: windowLocationPathname.slice(4),
+        reason_id: eventId,
+    };
 
     try {
-        let response = await fetch(fetchUrl);
+        let response = await fetch(fetchUrl, {
+            method: "POST",
+            headers: {
+                accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(body),
+        });
         if (response.ok) {
-            let eventList = await response.json();
-            let carEvents = await JSON.parse(eventList.contents);
-            carEventList = [...carEvents];
-
-            carEventsContainer.innerHTML = carEventList.map((carEvent) => addCarEventRadioBtn(carEvent)).join("");
-            submitBtn.removeAttribute("disabled", "");
-            console.log(carEventList);
+            let answer = await response.json();
+            if (answer.error_code === "SEND_TIMEOUT") {
+                showError(answer.error_message, "Чуть помедленнее");
+            } else {
+                notificationId = answer.notification_id;
+                openModal(sentMessegeNotice);
+                checkMsgStatus();
+            }
         } else {
-            console.log("Ошибка соединения с сервером: ", response.status);
+            showError(response.status);
         }
     } catch (err) {
-        console.log("Ошибка соединения с сервером: ", err);
+        showError(err);
     }
+}
+
+async function checkMsgStatus() {
+    const fetchUrl = `https://car-id.ru/api/notification/${notificationId}/status`;
+
+    try {
+        let fetchAnswer = await fetch(fetchUrl);
+        if (fetchAnswer.ok) {
+            let msgStatus = await fetchAnswer.json();
+
+            if (msgStatus.status !== "UNREAD" && msgStatus.status !== "READ") {
+                showError(msgStatus.status);
+            }
+            if (msgStatus.status === "UNREAD") {
+                sectionSendMsg.innerHTML = waitingForReadingNotice;
+                timeoutId = setTimeout(() => checkMsgStatus(), 5000);
+            }
+            if (msgStatus.status === "READ") {
+                clearTimeout(timeoutId);
+                sectionSendMsg.innerHTML = readMessegeNotice;
+            }
+        } else {
+            showError(response.status);
+        }
+    } catch (err) {
+        showError(err);
+    }
+}
+
+function showError(err, title) {
+    openModal(`
+        <div class="modal">
+            <div class="container">
+                <h2>${title ? title : "Сообщение об ошибке"}</h2>
+                <p>${err}</p>
+                ${title === undefined ? "<p>Попробуйте обновить страницу и отправить завпрос позднее.</p>" : ""}
+                <button id="close-btn" class="primary-btn">Понятно</button>
+            </div>
+        </div>
+    `);
+}
+
+function openModal(msgHtml) {
+    modalContainer.innerHTML = msgHtml;
+    const closeBtn = document.querySelector("#close-btn");
+    closeBtn.addEventListener("click", (event) => closeModal(event));
 }
 
 function closeModal(event) {
     event.preventDefault();
-    sectionSendMsg.innerHTML = "";
     modalContainer.innerHTML = "";
     window.scrollTo(0, 0);
 }
---
+++ js/systemMessages.js
@@ -0,0 +1,31 @@
+export const sentMessegeNotice = `
+    <div class="modal">
+        <div class="container">
+            <h2>Cообщение отправлено</h2>
+            <p>Владелец авто получит уведомление в приложение Car ID.</p>
+            <p>Ожидаем ответа от владельца автомобиля.</p>
+            <p>Не закрывайте браузер и разрешите отправку уведомлений.</p>
+            <button id="close-btn" class="primary-btn">Понятно</button>
+        </div>
+    </div>
+`;
+
+export const waitingForReadingNotice = `
+    <div class="notice">
+        <h2>Сообщение доставлено</h2>
+        <p>Ждём, когда прочитают.</p>
+        <div class="img">
+            <img src="./img/spinner.svg" width="75" width="75" alt="spinner" class="spinner"/>
+        </div>
+    </div>
+`;
+
+export const readMessegeNotice = `
+    <div class="notice">
+        <h2>Сообщение прочитано</h2>
+        <p>Ожидайте водителя.</p>
+        <div class="img">
+            <img src="./img/check-circle.svg" width="68" width="68" alt="check mark" />
+        </div>
+    </div>
+`;
