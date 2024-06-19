8766ea7edc6500cb97a2ccba4ffb8b44d4321545
Andrey <va_bank@mail.ru>
2024-06-19 16:48:01 +0300

    Add guid logic

--- js/script.js
+++ js/script.js
@@ -1,25 +1,28 @@
-import { sentMessageNotice } from "./systemMessages.js";
-
-const carEventsContainer = document.querySelector("#radio-btns");
-const submitBtn = document.querySelector("#submit");
 const modalContainer = document.querySelector(".modal-container");
-let carEventList = [];
-let notificationId = 0;
+const qrId = window.location.pathname.slice(4,40);
 
-buildEventList();
+checkQr();
 
-async function buildEventList() {
+async function checkQr() {
     if (window.location.pathname.includes("notification")) return;
-    const fetchUrl = "https://car-id.ru/api/report/get_all_reasons";
+
+    const fetchUrl = `https://car-id.ru/api/qr/${qrId}`;
 
     try {
         let response = await fetch(fetchUrl);
-        if (response.ok) {
-            carEventList = [...(await response.json())];
 
-            carEventsContainer.innerHTML = carEventList.map((carEvent) => createCarEventRadioBtn(carEvent)).join("");
-            submitBtn.removeAttribute("disabled", "");
-            submitBtn.addEventListener("click", (event) => readMsg(event), false);
+        if (response.ok) {
+            response = await response.json();
+
+            if (response.status === "ACTIVE") {
+                createMsg();
+                return;
+            }
+
+            if (response.error_message) {
+                showError(response.error_message);
+                return;
+            }
         } else {
             showError(response.status);
         }
@@ -28,28 +31,15 @@ async function buildEventList() {
     }
 }
 
-function createCarEventRadioBtn(carEvent) {
-    return `<label class="custom-radio">${carEvent.description}<input type="radio" name="carEvent" id=${carEvent.id} /></label>`;
-}
-
-function readMsg(event) {
-    event.preventDefault();
-    const userChoice = document.querySelector("input:checked");
-    if (userChoice === undefined || userChoice === null) return;
-    carEventList.map((event) => (+event.id === +userChoice.id ? sendMsg(event.id) : false));
-}
-
-async function sendMsg(eventId) {
-    // let windowLocationPathname = window.location.pathname;
-    const windowLocationPathname = "/qr/74b82e70-c612-4b7c-995a-e3bb88d3ad4a";
-    const fetchUrl = "https://car-id.ru/api/report/send";
-
-    let body = {
-        qr_id: windowLocationPathname.slice(4),
-        reason_id: eventId,
-    };
-
+async function createMsg() {
     try {
+        const fetchUrl = "https://car-id.ru/api/report/createDraft";
+
+        let body = {
+            qr_id: qrId,
+            reason_id: 1,
+        };
+
         let response = await fetch(fetchUrl, {
             method: "POST",
             headers: {
@@ -58,46 +48,48 @@ async function sendMsg(eventId) {
             },
             body: JSON.stringify(body),
         });
-        if (response.ok) {
-            let answer = await response.json();
 
-            if (answer.notification_id) {
-                notificationId = answer.notification_id;
-                localStorage.setItem("notification_id", `${notificationId}`);
-                openModal(sentMessageNotice, checkMsgStatus);
-            } else if (answer.error_code === "SEND_TIMEOUT") {
-                showError(answer.error_message, "Чуть помедленнее");
-            } else if (answer.error_code) {
-                showError(answer.error_message, "Упс... Уже исправляем");
-            } else showError(answer);
-        } else {
+        if (response.ok) {
+            response = await response.json();
+
+            if (![undefined, null, ""].includes(response.notification_id)) {
+                let notificationId = "";
+
+                if (window.location.host.includes("car-id.ru")) notificationId = response.notification_id;
+                if (!window.location.host.includes("car-id.ru")) {
+                    notificationId = "guid";
+                    sessionStorage.setItem("notificationId", response.notification_id);
+                }
+
+                history.replaceState({ page: 1 }, `${window.location.hostname}`, `/notification/${notificationId}`);
+                window.location.pathname = `./notification/${notificationId}`;
+                return;
+            }
             showError(response.status);
+        }
+
+        if (response.error_message) {
+            showError(response.error_message);
+            return;
         }
     } catch (err) {
         showError(err);
     }
 }
 
-function checkMsgStatus(event) {
-    closeModal(event);
-    const pathnamePrefix = window.location.host === 'andrey-trofimov.github.io' ? '/car-id' : ''; 
-    window.location.href = `${pathnamePrefix}/notification/737/`;
-    // window.location.href = `/notification/${notificationId}/`;
-}
-
-export function showError(err, title) {
+function showError(err, title) {
+    console.log(err);
     openModal(
         `
         <div class="modal">
             <div class="container">
                 <h2>${title ? title : "Сообщение об ошибке"}</h2>
-                <p>${err}</p>
-                ${title === undefined ? "<p>Попробуйте обновить страницу и отправить завпрос позднее.</p>" : ""}
+                <p>${err === undefined ? "<p>Попробуйте обновить страницу и отправить завпрос позднее.</p>" : err}</p>
                 <button id="close-btn" class="primary-btn">Понятно</button>
             </div>
         </div>
     `,
-        closeModal
+        closeErrorModal
     );
 }
 
@@ -107,8 +99,8 @@ export function openModal(msgHtml, cb) {
     closeBtn.addEventListener("click", (event) => cb(event));
 }
 
-export function closeModal(event) {
+function closeErrorModal(event) {
     event.preventDefault();
-    modalContainer.innerHTML = "";
-    window.scrollTo(0, 0);
+    history.replaceState({ page: 1 }, `${window.location.pathname}`, `/`);
+    window.location.pathname = "./";
 }
